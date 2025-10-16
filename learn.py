d7adf7f74dd53ad2c79ea91108006e5ff00694fe
Dwarika Kohar <dwarikakohar790@gmail.com>
2025-10-16 19:38:37 +0545

    Update learn.py
    turtle code

--- learn.py
+++ learn.py
@@ -1,1 +1,18 @@
-print("Hello, World!")
+import turtle as t
+import math
+import random
+t.bgcolor("black")
+t.speed(0)
+while True:
+    r = random.randint(1,50)
+    b = random.randint(1,80)
+    c = random.randint(1,30)
+    d = random.randint(1,10)
+#    for i in range(100)
+    t.pencolor("orange")
+    t.forward(r)
+    t.right(d)
+    t.circle(b)
+    t.forward(math.pi)
+    
+        
